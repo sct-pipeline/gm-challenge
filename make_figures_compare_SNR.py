3875ad6df45605e6fe7a15748b50a57c8ec30969
Julien Cohen-Adad <jcohen@polymtl.ca>
2018-10-11 14:28:38 -0400

    New function to generate SNR comparison fig

---
+++ make_figures_compare_SNR.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+#
+# Make figures to compare SNR methods: SNR_diff versus SNR_single.
+# This script will loop across all subjects, retrieve the results/results.csv and generate a plot.
+#
+# USAGE:
+# The script should be launched using SCT's python:
+#   ${SCT_DIR}/python/bin/python simu_make_figures_compare_SNR.py -i PATH_TO_DATA
+#
+# OUTPUT:
+# Fig
+#
