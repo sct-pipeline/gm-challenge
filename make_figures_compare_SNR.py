ef3ea0f024af4d55d7b74a8f0f6b111fd8b3f591
Nicolas P <nicolas.e.pinon@gmail.com>
2018-10-12 15:07:07 -0400

    Added a script to show SNR_diff VS SNR_single and up
    Added a script that outputs a figure shozing correlation between SNR_diff and SNR_single
    Updated readme
    Small correction in simu_make_figures description

--- make_figures_compare_SNR.py
+++ make_figures_compare_SNR.py
@@ -10,3 +10,54 @@
 # OUTPUT:
 # Fig
 #
+# Authors: Nicolas Pinon
+
+import os
+import argparse
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+def get_parameters():
+    parser = argparse.ArgumentParser(description='Generate a figure which compares the two SNR methods : SNR_single VS'
+                                                 'SNR_diff . Run after simu_process_data.py')
+    parser.add_argument("-i", "--input",
+                        help="List here the path to the data, which should include ",
+                        required=True)
+    args = parser.parse_args()
+    return args
+
+
+def main():
+
+    os.chdir(data_path)  # making the path provided by the user the cwd
+    SNR_diff,SNR_single  = [],[]
+
+    for folder in os.listdir('.'):  # going trough all folders
+        try:
+            os.chdir(data_path + '/' + folder + '/' + 'results')
+            # in each subjects folder's there should be a folder named 'results' and inside a csv file named 'results.csv'
+            results = pd.read_csv('results.csv')
+            SNR_diff.append(results.iat[0,1])
+            SNR_single.append(results.iat[2, 1])
+        except: # if there is something else than a subject's folder or if there is no csv results in the folder
+            pass
+
+
+    _, ax = plt.subplots(figsize=(6, 6))
+    ax.scatter(SNR_diff, SNR_single, c=".3", color='blue')
+    ax.plot(ax.get_xlim(), ax.get_ylim(), color='blue', ls="--", c=".3") # adding a y=x line to show correlation
+    plt.xlabel('SNR_diff', color='blue', fontsize=15)
+    plt.ylabel('SNR_single', color='blue', fontsize=15)
+    plt.title('Correlation between SNR_single and SNR_diff')
+
+    os.chdir(data_path)
+    plt.savefig('SNR_diff_VS_SNR_single.png')
+    plt.show()
+
+
+
+if __name__ == "__main__":
+    args = get_parameters()
+    data_path = args.input
+    main()
