17a716f6851ae1d4621f0ef75a9ca29f7a5255b1
Julien Cohen-Adad <jcohen@polymtl.ca>
2021-11-23 21:44:12 -0500

    Implemented metrics computation

--- simu_process_data.py
+++ simu_process_data.py
@@ -14,9 +14,7 @@
 
 import sys, os, shutil, argparse, pickle, io, glob
 from subprocess import call
-import numpy as np
-import pandas as pd
-import pandas as pd
+import pandas
 
 
 def get_parameters():
@@ -48,12 +46,33 @@ def get_parameters():
     return args
 
 
-def compute_metrics(file_1, file_2, file_wm, path_out):
+def compute_metrics(file_1, file_2, file_wm, file_gm, path_out):
     # Compute SNR using both methods
     call(f'sct_image -i {file_1} {file_2} -concat t -o {path_out}data_concat.nii.gz'.split(' '))
     call(f'sct_compute_snr -i {path_out}data_concat.nii.gz -method diff -m {file_wm} -o {path_out}snr_diff.txt'.
          split(' '))
-    return 0
+    snr_diff = float(open(f'{path_out}snr_diff.txt', 'r').readline())
+    call(f'sct_compute_snr -i {path_out}data_concat.nii.gz -method single -m {file_wm} -m-noise {file_wm} '
+         f'-rayleigh 0 -o {path_out}snr_single.txt'.split(' '))
+    snr_single = float(open(f'{path_out}snr_single.txt', 'r').readline())
+    # Compute average value in WM and GM on a slice-by-slice basis
+    call(f'sct_extract_metric -i {file_1} -f {file_wm} -method bin -o {path_out}signal_wm.csv'.split(' '))
+    call(f'sct_extract_metric -i {file_2} -f {file_wm} -method bin -o {path_out}signal_wm.csv -append 1'.split(' '))
+    call(f'sct_extract_metric -i {file_1} -f {file_gm} -method bin -o {path_out}signal_gm.csv'.split(' '))
+    call(f'sct_extract_metric -i {file_2} -f {file_gm} -method bin -o {path_out}signal_gm.csv -append 1'.split(' '))
+    # Compute contrast slicewise and average across slices
+    pd_gm = pandas.read_csv(f'{path_out}signal_gm.csv')
+    pd_wm = pandas.read_csv(f'{path_out}signal_wm.csv')
+    pd_contrast = abs(pd_gm['BIN()'] - pd_wm['BIN()']) / pandas.DataFrame([pd_gm['BIN()'], pd_wm['BIN()']]).min()
+    contrast = pd_contrast.mean()
+    # Build dict
+    dict_out = {
+        'SNR_single': snr_single,
+        'SNR_diff': float(open(f'{path_out}snr_diff.txt', 'r').readline()),
+        'Contrast': contrast,
+        'CNR': contrast * snr_single,
+        }
+    return dict_out
 
 
 def main():
@@ -66,22 +85,23 @@ def main():
     fname_csv_list = sorted(glob.glob(os.path.join(folder1, "*.csv")))
 
     # initialize dataframe
-    results_all = pd.DataFrame(columns={'WM',
-                                        'GM',
-                                        'Noise',
-                                        'Smooth',
-                                        'SNR',
-                                        'Contrast',
-                                        'Sharpness'})
+    results_all = pandas.DataFrame(columns={'WM',
+                                            'GM',
+                                            'Noise',
+                                            'Smooth',
+                                            'SNR',
+                                            'Contrast',
+                                            'Sharpness'})
 
     file_wm = os.path.join(folder1, 'mask_wm.nii.gz')
+    file_gm = os.path.join(folder1, 'mask_gm.nii.gz')
 
     # loop and process
     os.makedirs(path_output, exist_ok=True)
     i = 0
     for fname_csv in fname_csv_list:
         # get file name
-        metadata = pd.read_csv(fname_csv, index_col=0).to_dict()['0']
+        metadata = pandas.read_csv(fname_csv, index_col=0).to_dict()['0']
         file_data = metadata["File"]
         # get fname of each nifti file
         fname1 = os.path.join(folder1, file_data)
@@ -90,7 +110,7 @@ def main():
         print("\nData #1: " + fname1)
         print("Data #2: " + fname2)
         # process pair of data
-        results = compute_metrics(fname1, fname2, file_wm, path_output)
+        results = compute_metrics(fname1, fname2, file_wm, file_gm, path_output)
         # append to dataframe
         results_all = results_all.append({'WM': metadata['WM'],
                                           'GM': metadata['GM'],
