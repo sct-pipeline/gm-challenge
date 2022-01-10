a87377e3d7ee789f7660595089030a0b6468d408
Julien Cohen-Adad <jcohen@polymtl.ca>
2022-01-10 18:42:09 -0500

    Cleanup

--- gmchallenge/simu_process_data.py
+++ gmchallenge/simu_process_data.py
@@ -40,34 +40,6 @@ def run(cmd):
     call(cmd.split(' '), stdout=open(os.devnull, "w"))
 
 
-# TODO: remove
-# def compute_metrics(file_1, file_2, file_wm, file_gm, path_out):
-#     # Compute SNR using both methods
-#     run(f'sct_image -i {file_1} {file_2} -concat t -o {path_out}data_concat.nii.gz')
-#     run(f'sct_compute_snr -i {path_out}data_concat.nii.gz -method diff -m {file_wm} -o {path_out}snr_diff.txt')
-#     run(f'sct_compute_snr -i {path_out}data_concat.nii.gz -method single -m {file_wm} -m-noise {file_wm} -rayleigh 0 '
-#         f'-o {path_out}snr_single.txt')
-#     snr_single = float(open(f'{path_out}snr_single.txt', 'r').readline())
-#     # Compute average value in WM and GM on a slice-by-slice basis
-#     run(f'sct_extract_metric -i {file_1} -f {file_wm} -method wa -o {path_out}signal_wm.csv')
-#     run(f'sct_extract_metric -i {file_2} -f {file_wm} -method wa -o {path_out}signal_wm.csv -append 1')
-#     run(f'sct_extract_metric -i {file_1} -f {file_gm} -method wa -o {path_out}signal_gm.csv')
-#     run(f'sct_extract_metric -i {file_2} -f {file_gm} -method wa -o {path_out}signal_gm.csv -append 1')
-#     # Compute contrast slicewise and average across slices
-#     pd_gm = pandas.read_csv(f'{path_out}signal_gm.csv')
-#     pd_wm = pandas.read_csv(f'{path_out}signal_wm.csv')
-#     pd_contrast = 100 * abs(pd_gm['WA()'] - pd_wm['WA()']) / pandas.DataFrame([pd_gm['WA()'], pd_wm['WA()']]).min()
-#     contrast = pd_contrast.mean()
-#     # Build dict
-#     dict_out = {
-#         'SNR_single': snr_single,
-#         'SNR_diff': float(open(f'{path_out}snr_diff.txt', 'r').readline()),
-#         'Contrast': contrast,
-#         'CNR': contrast * snr_single,
-#         }
-#     return dict_out
-
-
 def main(argv=None):
     parser = get_parser()
     args = parser.parse_args(argv)
