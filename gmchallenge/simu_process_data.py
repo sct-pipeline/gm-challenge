009f120e5ac01ab83288de528b6f0b3699683133
Julien Cohen-Adad <jcohen@polymtl.ca>
2022-01-10 10:52:15 -0500

    Replaced processing with compute_cnr.py

--- gmchallenge/simu_process_data.py
+++ gmchallenge/simu_process_data.py
@@ -40,31 +40,32 @@ def run(cmd):
     call(cmd.split(' '), stdout=open(os.devnull, "w"))
 
 
-def compute_metrics(file_1, file_2, file_wm, file_gm, path_out):
-    # Compute SNR using both methods
-    run(f'sct_image -i {file_1} {file_2} -concat t -o {path_out}data_concat.nii.gz')
-    run(f'sct_compute_snr -i {path_out}data_concat.nii.gz -method diff -m {file_wm} -o {path_out}snr_diff.txt')
-    run(f'sct_compute_snr -i {path_out}data_concat.nii.gz -method single -m {file_wm} -m-noise {file_wm} -rayleigh 0 '
-        f'-o {path_out}snr_single.txt')
-    snr_single = float(open(f'{path_out}snr_single.txt', 'r').readline())
-    # Compute average value in WM and GM on a slice-by-slice basis
-    run(f'sct_extract_metric -i {file_1} -f {file_wm} -method wa -o {path_out}signal_wm.csv')
-    run(f'sct_extract_metric -i {file_2} -f {file_wm} -method wa -o {path_out}signal_wm.csv -append 1')
-    run(f'sct_extract_metric -i {file_1} -f {file_gm} -method wa -o {path_out}signal_gm.csv')
-    run(f'sct_extract_metric -i {file_2} -f {file_gm} -method wa -o {path_out}signal_gm.csv -append 1')
-    # Compute contrast slicewise and average across slices
-    pd_gm = pandas.read_csv(f'{path_out}signal_gm.csv')
-    pd_wm = pandas.read_csv(f'{path_out}signal_wm.csv')
-    pd_contrast = 100 * abs(pd_gm['WA()'] - pd_wm['WA()']) / pandas.DataFrame([pd_gm['WA()'], pd_wm['WA()']]).min()
-    contrast = pd_contrast.mean()
-    # Build dict
-    dict_out = {
-        'SNR_single': snr_single,
-        'SNR_diff': float(open(f'{path_out}snr_diff.txt', 'r').readline()),
-        'Contrast': contrast,
-        'CNR': contrast * snr_single,
-        }
-    return dict_out
+# TODO: remove
+# def compute_metrics(file_1, file_2, file_wm, file_gm, path_out):
+#     # Compute SNR using both methods
+#     run(f'sct_image -i {file_1} {file_2} -concat t -o {path_out}data_concat.nii.gz')
+#     run(f'sct_compute_snr -i {path_out}data_concat.nii.gz -method diff -m {file_wm} -o {path_out}snr_diff.txt')
+#     run(f'sct_compute_snr -i {path_out}data_concat.nii.gz -method single -m {file_wm} -m-noise {file_wm} -rayleigh 0 '
+#         f'-o {path_out}snr_single.txt')
+#     snr_single = float(open(f'{path_out}snr_single.txt', 'r').readline())
+#     # Compute average value in WM and GM on a slice-by-slice basis
+#     run(f'sct_extract_metric -i {file_1} -f {file_wm} -method wa -o {path_out}signal_wm.csv')
+#     run(f'sct_extract_metric -i {file_2} -f {file_wm} -method wa -o {path_out}signal_wm.csv -append 1')
+#     run(f'sct_extract_metric -i {file_1} -f {file_gm} -method wa -o {path_out}signal_gm.csv')
+#     run(f'sct_extract_metric -i {file_2} -f {file_gm} -method wa -o {path_out}signal_gm.csv -append 1')
+#     # Compute contrast slicewise and average across slices
+#     pd_gm = pandas.read_csv(f'{path_out}signal_gm.csv')
+#     pd_wm = pandas.read_csv(f'{path_out}signal_wm.csv')
+#     pd_contrast = 100 * abs(pd_gm['WA()'] - pd_wm['WA()']) / pandas.DataFrame([pd_gm['WA()'], pd_wm['WA()']]).min()
+#     contrast = pd_contrast.mean()
+#     # Build dict
+#     dict_out = {
+#         'SNR_single': snr_single,
+#         'SNR_diff': float(open(f'{path_out}snr_diff.txt', 'r').readline()),
+#         'Contrast': contrast,
+#         'CNR': contrast * snr_single,
+#         }
+#     return dict_out
 
 
 def main(argv=None):
@@ -82,9 +83,10 @@ def main(argv=None):
                                             'GM',
                                             'Noise',
                                             'Smooth',
-                                            'SNR',
-                                            'Contrast',
-                                            'CNR'})
+                                            'snr_single',
+                                            'snr_diff',
+                                            'cnr_single',
+                                            'cnr_diff'})
 
     file_wm = os.path.join(folder1, 'mask_wm.nii.gz')
     file_gm = os.path.join(folder1, 'mask_gm.nii.gz')
@@ -100,23 +102,21 @@ def main(argv=None):
         fname1 = os.path.join(folder1, file_data)
         fname2 = os.path.join(folder2, file_data)
         # process pair of data
-        results = compute_metrics(fname1, fname2, file_wm, file_gm, path_output)
-        # TODO: replace code above by code below.
-        # https://github.com/sct-pipeline/gm-challenge/issues/70
-        # results = compute_cnr.main(['--data1', fname1,
-        #                             '--data2', fname2,
-        #                             '--mask-noise', file_wm,
-        #                             '--mask-wm', file_wm,
-        #                             '--mask-gm', file_gm])
+        results = compute_cnr.main(['--data1', fname1,
+                                    '--data2', fname2,
+                                    '--mask-noise', file_wm,
+                                    '--mask-wm', file_wm,
+                                    '--mask-gm', file_gm])
         # append to dataframe
         results_all = results_all.append({'WM': metadata['WM'],
                                           'GM': metadata['GM'],
                                           'Noise': metadata['Noise'],
                                           'Smooth': metadata['Smooth'],
-                                          'SNR_single': results['SNR_single'],
-                                          'SNR_diff': results['SNR_diff'],
-                                          'Contrast': results['Contrast'],
-                                          'CNR': results['CNR']},ignore_index=True)
+                                          'snr_single': results['snr_single'],
+                                          'snr_diff': results['snr_diff'],
+                                          'cnr_single': results['cnr_single'],
+                                          'cnr_diff': results['cnr_diff']},
+                                         ignore_index=True)
         pbar.update(1)
     pbar.close()
     results_all.to_csv(file_output)
